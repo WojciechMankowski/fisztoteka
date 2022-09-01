d309bcb3d752598a4870e3f6eafad63b0aa7c8cc
WojciechMankowski <wojtekm510@gmail.com>
2022-09-01 08:25:09 +0200

    add index.js

---
+++ JS/index.js
@@ -0,0 +1,4 @@
+// TODO funkcja do pobierania nazw zestawów
+// TODO funkcja do zapisywania fiszek do zestawu
+// TODO funkcja do pobierania fiszek z konkretnej kategorii
+// TODO zmianna kategorii fiszek => uczę się - powtarzam - znam
