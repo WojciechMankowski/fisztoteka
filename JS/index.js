546e05c6f427aa59a3b982365d9b1942c813fab0
Wojciech M <wojtekm@myprograme.com>
2022-09-02 10:35:14 +0200

    add changes and logic to learn from flashcards

--- JS/index.js
+++ JS/index.js
@@ -1,81 +1,15 @@
-// TODO funkcja do pobierania nazw zestawów
+// funkcja do pobierania nazw zestawów
+import { getNameSet, addNewSet, creatOptions, addFidhCard } from './server_get.js';
+const URL = 'http://127.0.0.1:8000/';
 
-const URL = "http://127.0.0.1:8000/";
-// TODO pobranie nazw zestawów fiszek
-const name_set = [];
-const addNameSet = [];
-let next_click = 0;
-const getNameSet = () => {
-  const text = `wojtek/sets`;
-  const url = `${URL}${text}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) =>
-      data.forEach((element) => {
-        name_set.push(element.name_set);
-      })
-    )
-    .catch((error) => console.error(error));
-};
-const creatOptions = () => {
-  const elementSelect = document.querySelector("#nameset_select");
-  let length = elementSelect.options.length - 1;
-  console.log(next_click);
-  if (next_click == 0) {
-    name_set.forEach((element) => {
-      console.log(element);
-      elementSelect.options[length + 1] = new Option(element, element);
-      addNameSet.push(element);
-    });
-    next_click++;
-    length++;
-  } else {
-    console.log(addNameSet);
-    next_click++;
-    console.log(name_set);
-    name_set.forEach((element) => {
-      console.log(element in addNameSet);
-      if (element in addNameSet) {
-        elementSelect.options[length + 1] = new Option(element, element);
-        addNameSet.push(element);
-        length++;
-      }
-    });
-  }
-};
 
-// TODO funkcja do zapisywania fiszek do zestawu
-// TODO funkcja do zapisywania nowego zestawu
-const addNewSet = (event) => {
-  event.preventDefault();
-  const name_set = document.querySelector("#nameset").value;
-  console.log(name_set);
-  const text = `add_set/?user_name=wojtek&name_set=${name_set}`;
-  const url = `${URL}${text}`;
-  console.log(url);
-  fetch(url, { method: "POST" })
-    .then((res) => console.log(res))
-    .catch((error) => console.error(error));
-};
-// TODO funkcja do pobierania fiszek z konkretnej kategorii
 // TODO zmianna kategorii fiszek => uczę się - powtarzam - znam
-getNameSet();
 
-const elementSelect = document.querySelector("#nameset_select");
-const btn_addset = document.querySelector("#addset");
-// elementSelect.addEventListener("click", creatOptions);
-btn_addset.addEventListener("click", addNewSet);
-const creatOptions2 = () => {
-  const lengtNameSet = name_set.length;
-  const elementSelect = document.querySelector("#nameset_select");
-  let length = elementSelect.options.length - 1;
-  
-  let index = 0;
-  while (lengtNameSet != index) {
-	console.log(name_set[index]);
-	elementSelect.options[length + 1] = new Option(name_set[index], name_set[index]);
-	index++
-	length++
-  }
-};
-elementSelect.addEventListener("click", creatOptions2);
+
+getNameSet();
+const elementSelect = document.querySelector('#nameset_select');
+const btn_addset = document.querySelector('#addset');
+const btn_addCards = document.querySelector('#btn_addCart');
+btn_addset.addEventListener('click', addNewSet);
+elementSelect.addEventListener('click', creatOptions);
+btn_addCards.addEventListener('click', addFidhCard);
---
+++ JS/server_learn.js
@@ -0,0 +1,51 @@
+// TODO funkcja do pobierania fiszek z konkretnej kategorii
+import { creatOptions, getNameSet } from './server_get.js';
+
+const user_name = 'wojtek';
+const URL = 'http://127.0.0.1:8000/';
+
+const creatTextoLI = (flashcards) => {
+	const infoElement = document.createElement('div');
+	const contryName = document.createElement('strong');
+	const span = document.createElement('span');
+	const elemntBr = document.createComment('br');
+	span.innerHTML = flashcards.definition;
+	contryName.innerHTML = flashcards.notion;
+	infoElement.appendChild(contryName);
+	infoElement.appendChild(span);
+	return infoElement;
+};
+const creatLI = (flashcards) => {
+	const li = document.createElement('li');
+	li.appendChild(creatTextoLI(flashcards));
+	return li;
+};
+
+const creatUL = (flashcards) => {
+	const Elementlist = document.createElement('ul');
+	flashcards.forEach((card) => Elementlist.appendChild(creatLI(card)));
+	return Elementlist;
+};
+function functionLearn(flashcards) {
+	const box_learn = document.querySelector('.box_learn');
+	const elementList = creatUL(flashcards);
+	console.log(elementList);
+    box_learn.appendChild(elementList);
+}
+const doawlond_all_flashcards = (event) => {
+	event.preventDefault();
+	const elementSelectValue = document.querySelector('#nameset_select').value;
+	const text = `get_flashcard/${user_name}/${elementSelectValue}`;
+	const url = `${URL}${text}`;
+	fetch(url)
+		.then((res) => res.json())
+		.then((data) => {
+			functionLearn(data);
+		});
+};
+
+getNameSet();
+const elementSelect = document.querySelector('#nameset_select');
+const btn_learn = document.querySelector('.btn_learn');
+btn_learn.addEventListener('click', doawlond_all_flashcards);
+elementSelect.addEventListener('click', creatOptions);
