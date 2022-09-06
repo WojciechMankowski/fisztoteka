752eb0e7d3d14f19c3f29e8572ee604ea5bc07ec
Wojciech M <wojtekm@myprograme.com>
2022-09-06 12:59:47 +0200

    create label

--- my-app/src/components/Form/Input.tsx
+++ my-app/src/components/Form/Input.tsx
@@ -1,4 +1,4 @@
 import { InputProps } from "../../Types/props"
 export const Input = (props: InputProps) => {
-    return <input className="{props.class}" type={props.type}/>
+    return <input className={props.class} type={props.type}/>
 }
--- my-app/src/components/Form/Label.tsx
+++ my-app/src/components/Form/Label.tsx
@@ -1,7 +1,11 @@
-import {LabelProps} from '../../Types/props'
-import { Input } from './Input'
+import { LabelProps } from '../../Types/props';
+import { Input } from './Input';
 export const Label = (props: LabelProps) => {
-    return <label >{props.text}
-    <Input class={props.class} type={props.type} />
-    </label>
-}
+    const class_name = `${props.class}_label`
+	return (
+		<label className={class_name}>
+			{props.text}
+			<Input class={props.class} type={props.type} />
+		</label>
+	);
+};
